413e122d93bd49133d0ca9533b005e3b040c7e73
HDuc75 <nvhd86tdt@gmail.com>
2025-09-24 21:48:00 +0700

    Bài tập C++

---
+++ Bai 5 Luy Thua.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int a, b1, b2;
+	cin >> a;
+	b1 = pow(a,2);
+	b2 = pow(a,5);
+	cout << b1 << endl;
+	cout << b2 ;
+    return 0;
+}
---
+++ Bai 6 Can bac 2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	double a,b;
+	cin >> a;
+	b = sqrt(a);
+	cout << fixed << setprecision(2) << b;
+    return 0;
+}
+
