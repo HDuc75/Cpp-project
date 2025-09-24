ff33f0fc0abec221055a7d9e9db1176c1097061a
HDuc75 <nvhd86tdt@gmail.com>
2025-09-24 10:15:29 +0700

    Bài tập code

---
+++ Bai 2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long a,sum;
+    cin >> a;
+    sum = a + 2025;
+    cout << sum;
+    return 0;
+}
+
