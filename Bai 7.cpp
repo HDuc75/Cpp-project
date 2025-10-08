7b0a7d9f93c818e36c528abe1a29e39632927208
HDuc75 <nvhd86tdt@gmail.com>
2025-10-08 16:33:48 +0700

    Add files via upload

---
+++ Bai 7.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	double a,b;
+	cin >> a;
+	b = 1/a;
+	cout << fixed << setprecision(5) << b;
+    return 0;
+}
+
